fbc2cace711e1f2c00591b8ca542e6855bed3b64
yoonje <elxm6123@gmail.com>
2020-09-17 02:35:30 +0900

    remove security options

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -1,17 +1,26 @@
 package com.example.restfulwebservice.config;
 
+import com.sun.net.httpserver.HttpsConfigurator;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
-        throws Exception {
+            throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("kenneth")
                 .password("test1234")
